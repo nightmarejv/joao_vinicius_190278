fbe8dd53f9bd1fd92f3c06605cc5c5bfd3ed7bc1
nightmarejv <41236857+nightmarejv@users.noreply.github.com>
2021-06-24 20:35:57 -0300

    update

---
+++ app/src/main/java/com/example/joao_vinicius_190278_as/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.joao_vinicius_190278_as;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    FloatingActionButton fab = findViewById(R.id.fab);
+fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    });
+
+}
